41c2b1a9164458f44752a0e4729ff02ca97992bc
Matthew Zumwalt <matt@drolkar-2.local>
2009-08-24 12:12:11 -0500

    first commit - infusion 1.1.1 with uploader views

---
+++ init.rb
@@ -0,0 +1,3 @@
+# Include hook code here
+require "fluid_view_helpers" 
+ActionView::Base.send :include, FluidViewHelper
---
+++ test/fluid_view_helpers_test.rb
@@ -0,0 +1,8 @@
+require File.dirname(FILE) + '/test_helper.rb'
+include FluidViewHelper
+class ViewHelpersTest < Test::Unit::TestCase
+  def fluid_uploader_for_should_generate_javascript
+  end
+  def fluid_uploader_for_should_generate_html
+  end
+end
